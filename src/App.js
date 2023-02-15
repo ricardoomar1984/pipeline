f9981fa0782fe1e9ee4b97f49273155dd551794a
Ccadux <82245177+Ccadux@users.noreply.github.com>
2023-02-14 22:08:53 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-
+          
           Update EasyMoby 
         </p>
       </div>
