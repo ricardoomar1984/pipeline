f4c9378872c62d3cb5f3e72a93deb341c420cb0d
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2023-02-14 22:18:56 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ class App extends Component {
 
         </p>
           <h1 className="App-title">Welcome to FIAP 155GTI"</h1>
-        </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
           Made by: Gabriel, Marina and Victor
